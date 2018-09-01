0093ba98186c94d066f304c85fa3b84c47e16849
Gregor Richards <hg-yff@gregor.im>
2018-08-31 21:34:57 -0400

    Adding my own copyright lines would be wise...

--- examples/rnnoise_demo.c
+++ examples/rnnoise_demo.c
@@ -1,4 +1,5 @@
-/* Copyright (c) 2017 Mozilla */
+/* Copyright (c) 2018 Gregor Richards
+ * Copyright (c) 2017 Mozilla */
 /*
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
--- src/models.c
+++ src/models.c
@@ -1,4 +1,4 @@
-/* Copyright (c) Gregor Richards */
+/* Copyright (c) 2018 Gregor Richards */
 /*
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
