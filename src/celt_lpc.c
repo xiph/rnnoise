e14e01b6124b7f569f5c0f4e275e19ca68398aca
Joe Neeman <joeneeman@gmail.com>
2020-07-05 22:57:11 -0500

    Ported the first function!

--- src/celt_lpc.c
+++ src/celt_lpc.c
@@ -34,59 +34,11 @@
 #include "common.h"
 #include "pitch.h"
 
-void _celt_lpc(
+extern void _celt_lpc(
       opus_val16       *_lpc, /* out: [0...p-1] LPC coefficients      */
 const opus_val32 *ac,  /* in:  [0...p] autocorrelation values  */
 int          p
-)
-{
-   int i, j;
-   opus_val32 r;
-   opus_val32 error = ac[0];
-#ifdef FIXED_POINT
-   opus_val32 lpc[LPC_ORDER];
-#else
-   float *lpc = _lpc;
-#endif
-
-   RNN_CLEAR(lpc, p);
-   if (ac[0] != 0)
-   {
-      for (i = 0; i < p; i++) {
-         /* Sum up this iteration's reflection coefficient */
-         opus_val32 rr = 0;
-         for (j = 0; j < i; j++)
-            rr += MULT32_32_Q31(lpc[j],ac[i - j]);
-         rr += SHR32(ac[i + 1],3);
-         r = -SHL32(rr,3)/error;
-         /*  Update LPC coefficients and total error */
-         lpc[i] = SHR32(r,3);
-         for (j = 0; j < (i+1)>>1; j++)
-         {
-            opus_val32 tmp1, tmp2;
-            tmp1 = lpc[j];
-            tmp2 = lpc[i-1-j];
-            lpc[j]     = tmp1 + MULT32_32_Q31(r,tmp2);
-            lpc[i-1-j] = tmp2 + MULT32_32_Q31(r,tmp1);
-         }
-
-         error = error - MULT32_32_Q31(MULT32_32_Q31(r,r),error);
-         /* Bail out once we get 30 dB gain */
-#ifdef FIXED_POINT
-         if (error<SHR32(ac[0],10))
-            break;
-#else
-         if (error<.001f*ac[0])
-            break;
-#endif
-      }
-   }
-#ifdef FIXED_POINT
-   for (i=0;i<p;i++)
-      _lpc[i] = ROUND16(lpc[i],16);
-#endif
-}
-
+);
 
 int _celt_autocorr(
                    const opus_val16 *x,   /*  in: [0...n-1] samples x   */
@@ -118,28 +70,6 @@ int _celt_autocorr(
       xptr = xx;
    }
    shift=0;
-#ifdef FIXED_POINT
-   {
-      opus_val32 ac0;
-      ac0 = 1+(n<<7);
-      if (n&1) ac0 += SHR32(MULT16_16(xptr[0],xptr[0]),9);
-      for(i=(n&1);i<n;i+=2)
-      {
-         ac0 += SHR32(MULT16_16(xptr[i],xptr[i]),9);
-         ac0 += SHR32(MULT16_16(xptr[i+1],xptr[i+1]),9);
-      }
-
-      shift = celt_ilog2(ac0)-30+10;
-      shift = (shift)/2;
-      if (shift>0)
-      {
-         for(i=0;i<n;i++)
-            xx[i] = PSHR32(xptr[i], shift);
-         xptr = xx;
-      } else
-         shift = 0;
-   }
-#endif
    celt_pitch_xcorr(xptr, xptr, ac, fastN, lag+1);
    for (k=0;k<=lag;k++)
    {
@@ -147,26 +77,6 @@ int _celt_autocorr(
          d = MAC16_16(d, xptr[i], xptr[i-k]);
       ac[k] += d;
    }
-#ifdef FIXED_POINT
-   shift = 2*shift;
-   if (shift<=0)
-      ac[0] += SHL32((opus_int32)1, -shift);
-   if (ac[0] < 268435456)
-   {
-      int shift2 = 29 - EC_ILOG(ac[0]);
-      for (i=0;i<=lag;i++)
-         ac[i] = SHL32(ac[i], shift2);
-      shift -= shift2;
-   } else if (ac[0] >= 536870912)
-   {
-      int shift2=1;
-      if (ac[0] >= 1073741824)
-         shift2++;
-      for (i=0;i<=lag;i++)
-         ac[i] = SHR32(ac[i], shift2);
-      shift += shift2;
-   }
-#endif
 
    return shift;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,47 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use libc::c_int;
+
+#[no_mangle]
+pub extern "C" fn _celt_lpc(lpc: *mut f32, ac: *const f32, p: c_int) {
+    unsafe {
+        let lpc_slice = std::slice::from_raw_parts_mut(lpc, p as usize);
+        let ac_slice = std::slice::from_raw_parts(ac, p as usize + 1);
+        celt_lpc(lpc_slice, ac_slice);
+    }
+}
+
+fn celt_lpc(lpc: &mut [f32], ac: &[f32]) {
+    let p = lpc.len();
+    let mut error = ac[0];
+
+    for b in lpc.iter_mut() {
+        *b = 0.0;
+    }
+
+    if ac[0] == 0.0 {
+        return;
+    }
+
+    for i in 0..p {
+        // Sum up this iteration's reflection coefficient
+        let mut rr = 0.0;
+        for j in 0..i {
+            rr += lpc[j] * ac[i - j];
+        }
+        rr += ac[i + 1];
+        let r = -rr / error;
+        // Update LPC coefficients and total error
+        lpc[i] = r;
+        for j in 0..((i + 1) / 2) {
+            let tmp1 = lpc[j];
+            let tmp2 = lpc[i - 1 - j];
+            lpc[j] = tmp1 + r * tmp2;
+            lpc[i - 1 - j] = tmp2 + r * tmp1;
+        }
+
+        error = error - r * r * error;
+        // Bail out once we get 30 dB gain
+        if error < 0.001 * ac[0] {
+            return;
+        }
     }
 }
