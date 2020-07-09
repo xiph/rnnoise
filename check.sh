234388b00e1c60c01bc09f935d130f2a37bb2e96
Joe Neeman <joeneeman@gmail.com>
2020-07-08 23:27:21 -0500

    Remove remainder of the C code.

--- check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cargo build \
-    && make \
-    && ./examples/rnnoise_demo tests/testing.raw out.raw \
-    && cargo run --bin corr out.raw tests/reference_output.raw \
-    && echo "Check passed!"
